f56eca41ddf1cb8143c36f2725bab28d11d64490
tkddlf4209 <tkddlf4209@gmail.com>
2021-03-10 20:51:47 +0900

    B

---
+++ index.js
@@ -0,0 +1,103 @@
+
+var axios = require('axios').default;
+var express = require('express');
+var app = express();
+var cors = require("cors");
+app.use(cors());
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+const port = process.env.PORT || 3000;
+const TYPE_CRAWLER = 'crawler'; 
+const TYPE_BOT = 'bot';
+
+server.listen(port, async function () {
+  console.log(`application is listening on port@ ${port}...`);
+});
+
+var crawler_sockets = [];
+var bot_sockets = [];
+var running_crawler_socket = undefined;
+io.on("connection", (socket) => {
+
+  console.log("websocket connected ID : ", socket.id,'Type : ',socket.handshake.headers.type);
+
+  if(socket.handshake.headers.type == TYPE_CRAWLER){
+    crawler_sockets.push(socket);
+  }else{
+    bot_sockets.push(socket);
+  }
+
+  if(running_crawler_socket == undefined){
+    startCrawler(socket);
+  }
+
+  socket.on("notice", (rsp) => {
+    if(rsp.result == 'success'){
+      console.log(rsp.data);
+    }else{
+      deleteSocket(socket);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnect",socket.id);
+    deleteSocket(socket);
+  });
+});
+
+function deleteSocket(socket) {
+  
+  var position = crawler_sockets.indexOf(socket);
+  crawler_sockets.splice(position, 1);
+
+  position = bot_sockets.indexOf(socket);
+  bot_sockets.splice(position, 1);
+
+  if(socket.handshake.headers.type == TYPE_CRAWLER){
+    restartCrawlerServer(socket)
+  }
+
+  if(running_crawler_socket == socket){
+    startCrawler();
+  }
+}
+
+function startCrawler(){
+  if(crawler_sockets.length > 0){
+    running_crawler_socket = crawler_sockets[0];
+    io.to(running_crawler_socket.id).emit("start_crawler", { interval : 5000 });
+  }else{
+    running_crawler_socket = undefined;
+    console.log('crawler socket legnth 0');
+  }
+}
+
+
+const TOKEN = '17a48625-de4b-447c-ac52-1b2124b59878';
+function restartCrawlerServer(socket) {
+  var appName = socket.handshake.headers.app_name;
+  if(appName == undefined){
+    console.log('appName undefined');
+    return;
+  }
+
+  if(appName == 'APP_NAME_UNDEFINED'){
+    console.log('appName APP_NAME_UNDEFINED not allowed');
+    return;
+  }
+
+  axios({
+    method: 'delete',
+    url: 'https://api.heroku.com/apps/' + appName + '/dynos/',
+    timeout: 30000,
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/vnd.heroku+json; version=3',
+      'Authorization': 'Bearer ' + TOKEN
+    }
+  });
+}
+
+
+
+
