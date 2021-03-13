7681db428103f93a3f837b222a541b63e8d014ec
tkddlf4209 <tkddlf4209@gmail.com>
2021-03-13 20:54:53 +0900

    B

--- index.js
+++ index.js
@@ -148,8 +148,11 @@ function parsePosts(posts){
 
  function startCrawler(){
    if(Object.keys(crawler_sockets).length >0){
+     
      var first_crawler_socket_id = Object.keys(crawler_sockets)[0];
      running_crawler_socket_id = first_crawler_socket_id;
+     
+     console.log("cralwer_sockets : ",Object.keys(crawler_sockets).length, crawler_sockets,running_crawler_socket_id);
      io.to(first_crawler_socket_id).emit("start_crawler", { interval : 350 });
    }else{
     running_crawler_socket_id = -1;
