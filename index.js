436257ee7e70d202566b97801e251e848e01c37e
tkddlf4209 <tkddlf4209@gmail.com>
2021-03-19 12:10:55 +0900

    B

--- index.js
+++ index.js
@@ -71,7 +71,6 @@ function parsePosts(posts){
       var notice_id = posts[i].id;
       var notice_title = posts[i].text;
       if (notice_id != undefined && notice_title != undefined) {
-              
         if(notice_id==582){
             notice_id= 1
         }
@@ -81,14 +80,12 @@ function parsePosts(posts){
         if (check_title_from_id == undefined && check_title_from_title == undefined) { // 신규프로젝트 공시 등장
             var today = checkToday(posts[i].start_date.split("T")[0]) // 알림 발생 시간이 오늘인지 검사
             if(today){ // 오늘자 공시 만 알림 발생
-              if(notice_id > init_max_notice_id){ // 최초 init 시 notice_id 최대 공지 사항 아이디 보다 큰 경우 만 알림 발생
                 console.log('프로젝트감지 ',posts[i]);
                 posts[i].text = "("+posts[i].assets+")"+posts[i].text; // 타이틀 앞에 심볼 값 추가
                 Object.keys(bot_sockets).forEach(function(socket_id){
                   io.to(socket_id).emit('new_post',posts[i])
                 })
                 //fcm.sendUpbitProjectExchangeFCM(posts[i],posts[i].text);
-              }
             }else{
               console.log('!!!! new post is not today notice !!!!!',posts[i]);
             }
@@ -101,17 +98,11 @@ function parsePosts(posts){
   }else{
     for (var i = 0; i < posts.length; i++) {
       var notice_id = posts[i].id;
-      posts[i].text = "("+posts[i].assets+")"+posts[i].text; // 타이틀 앞에 심볼 값 추가
       var notice_title = posts[i].text;
-      var today = checkToday(posts[i].start_date.split("T")[0]) // 알림 발생 시간이 오늘인지 검사
-          
       if (notice_id != undefined && notice_title != undefined) {
         titles.set(notice_title,notice_id);
         ids.set(notice_id, notice_title);
-        init_max_notice_id = Math.max(init_max_notice_id,notice_id); // init 처음 가장 큰 notice id를 저장 // 이 id보다 작은 경우 무시.
       }
-
-      
     }
 
     console.log(titles);
